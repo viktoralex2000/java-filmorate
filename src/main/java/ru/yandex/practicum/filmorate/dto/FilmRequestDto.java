f0bea43a9d15bb7854ebd4bc5b240e5bd8ae7900
viktoralex2000 <viktoralex2000@gmail.com>
2026-02-03 19:21:19 +0400

    12 sprint is complete 1.19.6 linkedhashset genres2

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmRequestDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmRequestDto.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Data
