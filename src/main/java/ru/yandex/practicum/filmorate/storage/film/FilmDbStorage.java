07d4f79a0edb7e766e1ab2f8cb22b2c6c5483e35
viktoralex2000 <viktoralex2000@gmail.com>
2026-02-05 18:47:43 +0400

    12 sprint is complete 1.27.4

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -25,6 +25,16 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public long addFilm(Film film) {
+        if (film.getMpa() == null || getMpaById(film.getMpa().getId()) == null) {
+            throw new NotFoundException("MPA с id=" + (film.getMpa() != null ? film.getMpa().getId() : "null") + " не найден");
+        }
+        if (film.getGenres() != null) {
+            for (Genre genre : film.getGenres()) {
+                if (getGenreById(genre.getId()) == null) {
+                    throw new NotFoundException("Жанр с id=" + genre.getId() + " не найден");
+                }
+            }
+        }
         String sql = """
                 INSERT INTO films (film_name, description, release_date, duration, mpa_rating_id)
                 VALUES (?, ?, ?, ?, ?)
